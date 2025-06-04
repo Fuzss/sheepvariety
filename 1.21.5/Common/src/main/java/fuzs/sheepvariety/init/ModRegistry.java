3c9380023fe05216a2510cc5902a23bf46761572
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-06-04 18:08:54 +0200

    attempt fix rare render crash

--- 1.21.5/Common/src/main/java/fuzs/sheepvariety/init/ModRegistry.java
+++ 1.21.5/Common/src/main/java/fuzs/sheepvariety/init/ModRegistry.java
@@ -8,11 +8,9 @@ import fuzs.sheepvariety.world.entity.animal.sheep.SheepVariant;
 import fuzs.sheepvariety.world.entity.animal.sheep.SheepVariants;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
-import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
 
 public class ModRegistry {
     public static final ResourceKey<Registry<SheepVariant>> SHEEP_VARIANT_REGISTRY_KEY = ResourceKey.createRegistryKey(
@@ -26,9 +24,6 @@ public class ModRegistry {
             SheepVariants::bootstrap);
 
     public static final DataAttachmentType<Entity, Holder<SheepVariant>> SHEEP_VARIANT_ATTACHMENT_TYPE = DataAttachmentRegistry.<Holder<SheepVariant>>entityBuilder()
-            .defaultValue((Entity entity) -> entity.getType() == EntityType.SHEEP, (RegistryAccess registries) -> {
-                return registries.lookupOrThrow(SHEEP_VARIANT_REGISTRY_KEY).getOrThrow(SheepVariants.DEFAULT);
-            })
             .persistent(SheepVariant.CODEC)
             .networkSynchronized(SheepVariant.STREAM_CODEC, PlayerSet::nearEntity)
             .build(SheepVariety.id("sheep_variant"));
